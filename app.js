32d79a6ad0430987723991bf92c2f5e8c7886041
[name] <lnginyard@gmail.com>
2020-10-19 15:54:37 -0400

    anagrams assessment completed

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// Your Code Here
+
+// let myDiv = document.createElement("div");
+// myDiv.textContent = getAnagramsOf;
+// document.body.append(myDiv);
+const button = document.getElementById("findButton");
+button.onclick = function () {
+  let typedText = document.getElementById("input").value;
+  function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+  }
+  let alphaText = alphabetize(typedText);
+  for (let aWord of words) {
+    if (alphabetize(aWord) === alphaText) {
+      console.log(aWord);
+      let p = document.createElement("p");
+      p.textContent = aWord;
+      document.body.append(p);
+    }
+    
+    // console.log("typedText", typedText);
+    // console.log("alphabetized text", alphabetize(typedText));
+    // console.log (alphaText, typedText)
+    // Make an array and add the words to the array,
+    // display array
+
+    // or append each word one at a time on the DOM
+  }
+};
+
+
