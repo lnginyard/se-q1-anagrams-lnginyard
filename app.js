bf902012055091405263523e3c45be7c1016f0b0
lnginyard <lnginyard@gmail.com>
2022-06-13 01:22:21 -0400

    correct spelling in js file

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
+const feathers = require('feathersjs/feathers');
+const app = feather();
+
 const button = document.getElementById("findButton");
 
-button.onclick = function () {
-  let typedText = document.getElementById("input").value;
-  getAnagramsOf(typedText);
+button.onclick = function() {
+    let typedText = document.getElementById("input").value;
+    getAnagramsOf(typedText);
 };
-function alphebetize(word) {
-  return word.toLowerCase().split("").sort().join("").trim();
+
+function alphabetize(word) {
+    return word.toLowerCase().split("").sort().join("").trim();
 }
 let anagrams = [];
+
 function getAnagramsOf(typedText) {
-  let currentWord = alphebetize(typedText);
-  for (let i = 0; i < words.length; i++) {
-    let nextWord = alphebetize(words[i]);
-    if (currentWord === nextWord) {
-      anagrams.push(words[i]);
+    let currentWord = alphabetize(typedText);
+    for (let i = 0; i < words.length; i++) {
+        let nextWord = alphabetize(words[i]);
+        if (currentWord === nextWord) {
+            anagrams.push(words[i]);
+        }
     }
-  }
-  let outputtedData = document.getElementById("listPerms");
-  for (i = 0; i < anagrams.length; i++) {
-    outputtedData.innerHTML += anagrams[i] + " ";
-  }
+    let outputtedData = document.getElementById("listPerms");
+    for (i = 0; i < anagrams.length; i++) {
+        outputtedData.innerHTML += anagrams[i] + " ";
+    }
 }
