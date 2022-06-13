3019794e93de31b39c96fe59759e015f39f936ec
lnginyard <lnginyard@gmail.com>
2022-06-13 09:58:42 -0400

    revert js file back to original in order for app to work properly as it did before.

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const feathers = require('feathersjs/feathers');
-const app = feather();
-
 const button = document.getElementById("findButton");
 
 button.onclick = function() {
@@ -8,15 +5,15 @@ button.onclick = function() {
     getAnagramsOf(typedText);
 };
 
-function alphabetize(word) {
+function alphebetize(word) {
     return word.toLowerCase().split("").sort().join("").trim();
 }
 let anagrams = [];
 
 function getAnagramsOf(typedText) {
-    let currentWord = alphabetize(typedText);
+    let currentWord = alphebetize(typedText);
     for (let i = 0; i < words.length; i++) {
-        let nextWord = alphabetize(words[i]);
+        let nextWord = alphebetize(words[i]);
         if (currentWord === nextWord) {
             anagrams.push(words[i]);
         }
